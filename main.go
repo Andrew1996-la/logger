3c2e96d73ca5a4c13d04fd0372fea6f557118991
Andrew <asoborev@mail.ru>
2026-04-06 21:30:31 +0300

    base logger

--- main.go
+++ main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"go.uber.org/zap"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world"))
-}
-
-func errorHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Something went wrong", http.StatusInternalServerError)
-}
+var logger *zap.Logger
 
 func main() {
+	logger, _ = zap.NewProduction()
+	defer logger.Sync()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/error", errorHandler)
 
-	fmt.Println("Server start on 8080 port")
+	logger.Info("Server started", zap.String(
+		"port", "8080",
+	))
+
 	http.ListenAndServe(":8080", mux)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+	logger.Info("hello handler",
+		zap.String("method", r.Method),
+		zap.String("path", r.URL.Path),
+	)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	logger.Error("error handler",
+		zap.String("method", r.Method),
+	)
 }
